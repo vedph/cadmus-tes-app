e2b3ba7ad40ba05886c8bdca3617e47da949ce3f
Myrmex <dfusi@hotmail.com>
2026-01-30 21:29:29 +0100

    Docker

--- public/env.js
+++ public/env.js
@@ -3,7 +3,7 @@
   window.__env = window.__env || {};
 
   // environment-dependent settings
-  window.__env.apiUrl = "http://localhost:5172/api/";
+  window.__env.apiUrl = "http://localhost:5101/api/";
   window.__env.version = "0.0.1";
   // enable thesaurus import in thesaurus list for admins
   window.__env.thesImportEnabled = true;
